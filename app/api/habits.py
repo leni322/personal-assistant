fe40c0fe8dff436794a3ab1a4b4f4e10e452125c
leni322 <malovik.m@mail.ru>
2025-09-08 16:19:50 +0300

    Added new queries

--- app/api/habits.py
+++ app/api/habits.py
@@ -1,34 +1,55 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
-from app import schemas, db, models
-from app.models import Habit, Base
+from app import models
 from app.db import SessionLocal, engine
-
-router = APIRouter()
+from app.schemas.habit_sh import Habit, HabitCreate, HabitUpdate
 
 models.Base.metadata.create_all(bind=engine)
 
+router = APIRouter()
+
 def get_db():
+    db = SessionLocal()
     try:
         yield db
     finally:
         db.close()
 
 
-@router.post("/habits/", response_model=schemas.Habit)
-def create_habit(habit: schemas.HabitCreate, db: Session = Depends(get_db)):
-    db_habit = models.habit.Habit(title=habit.title, description=habit.description)
+@router.post("/habits/", response_model=Habit)
+def create_habit(habit: HabitCreate, db: Session = Depends(get_db)):
+    db_habit = models.Habit(**habit.dict())
     db.add(db_habit)
     db.commit()
     db.refresh(db_habit)
     return db_habit
 
-@router.get("/habits/", response_model=list[schemas.Habit])
-def get_habits(db: Session = Depends(get_db)):
-    return db.query(models.habit.Habit).all()
+
+@router.get("/habits/", response_model=list[Habit])
+def read_habits(db: Session = Depends(get_db)):
+    return db.query(models.Habit).all()
 
 
+@router.put("/habits/{habit_id}", response_model=Habit)
+def update_habit(habit_id: int, habit: HabitUpdate, db: Session = Depends(get_db)):
+    db_habit = db.query(models.Habit).filter(models.Habit.id == habit_id).first()
+    if not db_habit:
+        raise HTTPException(status_code=404, detail="Habit not found")
 
-@router.get("/habits")
-def get_habits():
-    return [{"id": 1, "title": "Зарядка"}, {"id": 2, "title": "Чтение"}, {"id": 3, "title": "Программирование"}]
+    for key, value in habit.dict(exclude_unset=True).items():
+        setattr(db_habit, key, value)
+
+    db.commit()
+    db.refresh(db_habit)
+    return db_habit
+
+
+@router.delete("/habits/{habit_id}")
+def delete_habit(habit_id: int, db: Session = Depends(get_db)):
+    db_habit = db.query(models.Habit).filter(models.Habit.id == habit_id).first()
+    if not db_habit:
+        raise HTTPException(status_code=404, detail="Habit not found")
+
+    db.delete(db_habit)
+    db.commit()
+    return {"message": "Habit deleted"}
--- app/schemas/habit_sh.py
+++ app/schemas/habit_sh.py
@@ -7,6 +7,10 @@ class HabitBase(BaseModel):
 class HabitCreate(HabitBase):
     pass
 
+class HabitUpdate(BaseModel):
+    title: str | None = None
+    description: str | None = None
+
 class Habit(HabitBase):
     id: int
 
